ee709b1893fd4073220aa951fe9f4a1595f62055
cc-student <cc-student@ccstudent-HP-250-G6-Notebook-PC>
2021-01-18 22:01:59 +0100

    first commit from Eslipse Linux

---
+++ SCJP_again/src/de/naumovs/page87/primitives/Primitives.java
@@ -0,0 +1,19 @@
+package de.naumovs.page87.primitives;
+
+public class Primitives {
+	
+	byte b; // 2^(8 - sign) -> 2^7
+	short s; // 2bytes -> 16bits -> 2^(16bit-sign) > 2^15
+	int i; // 4bytes -> 32bits -> 2^(32bit-sign) > 2^31
+	long l; // 8bytes -> 64bits -> 2^(64bit-sign) > 2^63
+	
+	float f; // 4 bytes -> 32bits
+	double d; // 8 bytes -> 64bits
+	
+	
+	boolean myBoolenPrimitive;
+	int x,y,z;
+	
+	
+
+}
