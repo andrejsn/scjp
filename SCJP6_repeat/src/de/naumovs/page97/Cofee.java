d51f65e892350c53c472cc0046deec9305778fa6
Andrejs Naumovs <andrejs@naumovs.de>
2021-01-19 19:18:50 +0100

    page 101/890

---
+++ SCJP6_repeat/src/de/naumovs/page97/Cofee.java
@@ -0,0 +1,20 @@
+package de.naumovs.page97;
+
+public class Cofee {
+	
+
+	
+	CofeeSize size = CofeeSize.OVERWHELMING;
+	
+}
+
+enum CofeeSize {
+	BIG, HUGE, OVERWHELMING;
+}
+
+ class CoffeeTest1 {
+	public static void main(String[] args) {
+		Cofee cofee = new Cofee();
+		cofee.size = CofeeSize.HUGE;
+	}
+}
---
+++ SCJP6_repeat/src/de/naumovs/page97/enumm/CofeeSize.java
@@ -0,0 +1,5 @@
+package de.naumovs.page97.enumm;
+
+ public enum CofeeSize {
+	BIG, HUGE, OVERWHELMING;
+}
