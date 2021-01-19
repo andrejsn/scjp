43a0008f350bdefdc9fef37a3a07df79d7a55bf8
Andrejs Naumovs <andrejs@naumovs.de>
2021-01-19 15:06:48 +0100

    added page 94

---
+++ SCJP6_repeat/src/de/naumovs/page94/Array.java
@@ -0,0 +1,16 @@
+package de.naumovs.page94;
+
+public class Array {
+
+	int[] key;
+	int keys[];
+
+	Thread[] thread;
+	Thread threads[];
+
+	String[][][] occupantName;
+	String[] managerName[];
+	
+	
+	int[] scores;
+}
